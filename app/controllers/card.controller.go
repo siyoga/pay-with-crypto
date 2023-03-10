028d32283d264ccde40d4fe6f9ce6183fc8a3ffd
Sergey Dyakov <s.dyakov9@gmail.com>
2023-03-11 01:04:12 +0300

    fix(cards): add middlewares from todo

--- app/controllers/card.controller.go
+++ app/controllers/card.controller.go
@@ -13,10 +13,10 @@ func CardController(a *fiber.App) {
 	route.Get("/search", handlers.CardSearcherByNameHandler)
 	route.Get("/search/tags", handlers.CardsSearcherByTagsHandler)
 	route.Get("/search/id", handlers.CardsSearcherByIdHandler)
-	route.Get("/getForApprove", handlers.GetCardsForApprove) //TODO!: add middleware.AuthAdmin
+	route.Get("/getForApprove", middleware.AuthAdmin, handlers.GetCardsForApprove)
 	route.Post("/uploadLogo", middleware.Auth, handlers.CardLogoUploaderHandler)
 	route.Post("/create", middleware.Auth, handlers.CardCreatorHandler)
-	route.Post("/createTag", handlers.TagCreateHandler) //TODO!: add middleware.AuthAdmin
+	route.Post("/createTag", middleware.AuthAdmin, handlers.TagCreateHandler)
 	route.Delete("/delete", middleware.Auth, handlers.CardDeleteHandler)
 	route.Put("/edit", middleware.Auth, handlers.CardEditHandler)
 }
