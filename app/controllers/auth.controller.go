a93cfc1c724a732870198e299e7c7ce9c1965a4b
Sergey Dyakov <s.dyakov9@gmail.com>
2023-03-10 23:43:42 +0300

    feat(auth): Add middleware to admin register

--- app/controllers/auth.controller.go
+++ app/controllers/auth.controller.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"pay-with-crypto/app/handlers"
+	"pay-with-crypto/app/middleware"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -11,6 +12,6 @@ func AuthController(a *fiber.App) {
 
 	route.Post("/register", handlers.RegisterHandler)
 	route.Post("/login", handlers.LoginHandler)
-	route.Post("/admin_register", handlers.AdminRegisterHandler)
+	route.Post("/admin_register", middleware.AuthAdmin, handlers.AdminRegisterHandler)
 	route.Post("/admin_login", handlers.AdminLoginHandler)
 }
