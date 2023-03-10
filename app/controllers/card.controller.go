3aeb5a61989ddaeeb7676a6c644d8f2bc02974ca
Sergey Dyakov <s.dyakov9@gmail.com>
2023-03-11 01:10:00 +0300

    fix(cards): delete trash

--- app/controllers/card.controller.go
+++ app/controllers/card.controller.go
@@ -13,10 +13,8 @@ func CardController(a *fiber.App) {
 	route.Get("/search", handlers.CardSearcherByNameHandler)
 	route.Get("/search/tags", handlers.CardsSearcherByTagsHandler)
 	route.Get("/search/id", handlers.CardsSearcherByIdHandler)
-	route.Get("/getForApprove", middleware.AuthAdmin, handlers.GetCardsForApprove)
 	route.Post("/uploadLogo", middleware.Auth, handlers.CardLogoUploaderHandler)
 	route.Post("/create", middleware.Auth, handlers.CardCreatorHandler)
-	route.Post("/createTag", middleware.AuthAdmin, handlers.TagCreateHandler)
 	route.Delete("/delete", middleware.Auth, handlers.CardDeleteHandler)
 	route.Put("/edit", middleware.Auth, handlers.CardEditHandler)
 }
