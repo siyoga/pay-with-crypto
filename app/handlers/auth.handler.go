546247b4d939a88e692cf575e8070cf6273564b6
Krenras <rar3214@gmail.com>
2023-08-19 20:10:26 +0300

    fix(google/register): small fixes

--- app/handlers/auth.handler.go
+++ app/handlers/auth.handler.go
@@ -10,13 +10,10 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofrs/uuid"
 	"github.com/golang-jwt/jwt"
-	"github.com/grokify/go-pkce"
-	"github.com/sethvargo/go-password/password"
 
 	"net/mail"
 
 	"golang.org/x/crypto/bcrypt"
-	"golang.org/x/oauth2"
 )
 
 // @Description Create company.
@@ -149,91 +146,6 @@ func UpdateTokensHandler(c *fiber.Ctx) error {
 
 }
 
-func AuthGoogleGetApprove(c *fiber.Ctx) error {
-	path := googleutil.ConfigGoogle()
-
-	NewPKCE := *googleutil.CreatePKCE()
-
-	url := path.AuthCodeURL("state",
-		oauth2.SetAuthURLParam(pkce.ParamCodeChallenge, NewPKCE.CodeChallenge),
-		oauth2.SetAuthURLParam(pkce.ParamCodeChallengeMethod, pkce.MethodS256)) //TODO!:CHANGE STATE TO SOMETHING MORE SECURITY STRONG. In theory it should be random each time, but not sure.
-	return c.Redirect(url)
-}
-
-func Callback(c *fiber.Ctx) error {
-	var refreshToken db.RefreshToken
-
-	state := c.FormValue("state")
-	if state != "state" {
-		return c.Status(fiber.StatusBadRequest).JSON(utility.Message{Text: "Missing or malformed URI"})
-	}
-
-	code := c.FormValue("code")
-	PCKECode := googleutil.ThePKCE.CodeVerifier
-
-	tokens, err := googleutil.GetTokens(code, PCKECode)
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-	UserData, err := googleutil.GetUserData(tokens)
-	if err != nil {
-		return err
-	}
-
-	UserDataFromDb, check := db.GetOneBy[db.Company]("mail", UserData.Email)
-	if check {
-		response, err := AuthGoogleLoginUser(c, UserDataFromDb)
-		if err != nil {
-			return c.Status(fiber.StatusInternalServerError).JSON(utility.Message{Text: "Something’s wrong with the server. Try it later."})
-		}
-		return c.Status(fiber.StatusOK).JSON(response)
-	}
-
-	_, err = mail.ParseAddress(UserData.Email)
-	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(utility.Message{Text: "Something’s wrong with the server. Try it later."})
-	}
-	res, err := password.Generate(64, 10, 10, false, false)
-	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(utility.Message{Text: "Something’s wrong with the server. Try it later."})
-	}
-
-	user := db.Company{
-		ID:       uuid.Nil,
-		Username: UserData.Name,
-		Image:    UserData.Picture,
-		Password: res,
-		Mail:     UserData.Email,
-	}
-	user.ID = uuid.Must(uuid.NewV4())
-
-	hash, err := bcrypt.GenerateFromPassword([]byte(res), 12)
-	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(utility.Message{Text: "Something’s wrong with the server. Try it later."})
-	}
-	user.Password = string(hash)
-
-	if ok := db.Add(user); !ok {
-		return fiber.ErrInternalServerError
-	}
-
-	response, errs := generateTokenResponse(user.ID)
-	if errs[0] != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(utility.Message{Text: "Something’s wrong with the server. Try it later."})
-	}
-	if errs[1] != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(utility.Message{Text: "Something’s wrong with the server. Try it later."})
-	}
-
-	refreshToken.Token = response.RefreshToken
-
-	if ok := db.Add(refreshToken); !ok {
-		return c.Status(fiber.StatusInternalServerError).JSON(utility.Message{Text: "Something’s wrong with the server. Try it later."})
-	}
-
-	return c.Status(fiber.StatusOK).JSON(response)
-}
-
 func AuthGoogleLoginUser(c *fiber.Ctx, userdata db.Company) (utility.JWTTokenPair, error) {
 	var refreshToken db.RefreshToken
 
@@ -285,7 +197,7 @@ func WhoAmIHandler(c *fiber.Ctx) error {
 	return c.JSON(company)
 }
 
-func TokenInfo(c *fiber.Ctx) error {
+func GoogleRegisterHandler(c *fiber.Ctx) error {
 	var company db.Company
 
 	emptyErr := googleutil.GoogleErrorResponse{}
